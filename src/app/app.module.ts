eba0cff4cd0e1e4b50889779b409aa434a2773ae
Ali <a.ebrahimzadeh@nexeven.com>
2018-04-11 12:42:06 +0430

    Adding TSLint to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { I18nPipe } from '../utils/i18n/i18n-pipe'
+import { I18nPipe } from '../utils/i18n/i18n-pipe';
 import { AppComponent } from './app.component';
 
 
--- src/utils/i18n/i18n-pipe.ts
+++ src/utils/i18n/i18n-pipe.ts
@@ -8,10 +8,12 @@ export class I18nPipe implements PipeTransform {
     transform(key: String, ...replacements: String[]) {
         let result = messages.get(`${key}.${localeId}`) || ''
         replacements.forEach((replaceBy) => {
+
+            
             if (replaceBy != undefined) {
                 result = result.replace("${}", replaceBy.toString())
             } else {
-                result = result.replace("${}", '-----')
+                result =      result.replace("${}", '-----'); 
             }
         })
         return result
